db9875c9a84381a3fb34740fbbb015e951fbe99e
amankumar22805-sys <amankumar22805@gmail.com>
2026-06-05 11:19:43 +0530

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,32 +1,63 @@
 import {BrowserRouter,Routes,Route} from "react-router-dom";
+
 import Home from "./pages/Home";
 import Designer from "./pages/Designer";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
+import Login from "./pages/Login";
+
 import Navbar from "./components/Navbar";
+
 
 function App(){
 
 return(
+
 <BrowserRouter>
 
 <Navbar/>
 
 <Routes>
 
-<Route path="/" element={<Home/>}/>
 
-<Route path="/designer" element={<Designer/>}/>
+<Route 
+path="/" 
+element={<Home/>}
+/>
 
-<Route path="/cart" element={<Cart/>}/>
 
-<Route path="/checkout" element={<Checkout/>}/>
+<Route 
+path="/designer" 
+element={<Designer/>}
+/>
+
+
+<Route 
+path="/cart" 
+element={<Cart/>}
+/>
+
+
+<Route 
+path="/checkout" 
+element={<Checkout/>}
+/>
+
+
+<Route 
+path="/login" 
+element={<Login/>}
+/>
+
 
 </Routes>
 
+
 </BrowserRouter>
+
 )
 
 }
+
 
 export default App;
