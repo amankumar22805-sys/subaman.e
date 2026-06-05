2232b4cfbe4b6019e6f54d5aec3e4290cd488de1
amankumar22805-sys <amankumar22805@gmail.com>
2026-06-05 11:03:25 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import {ShoppingCart} from "lucide-react";
 export default function Navbar(){
 
 return(
-<nav className="bg-pink-600 text-white p-4 flex justify-between">
+<nav className="bg-pink-600 text-white p-4 flex justify-between items-center">
 
 <h1 className="text-2xl font-bold">
 SabAman.e
@@ -14,9 +14,17 @@ className="w-1/2 p-2 rounded text-black"
 placeholder="Search T-shirt, Mug, Cap..."
 />
 
-<div className="flex gap-5">
-Login
+<div className="flex gap-5 items-center">
+
+<a href="/">
+Home
+</a>
+
+<a href="/cart" className="flex gap-1">
+Cart
 <ShoppingCart/>
+</a>
+
 </div>
 
 </nav>
