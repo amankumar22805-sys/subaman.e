7611d62563b013dbb6a7b3d57877afb6478d14e6
amankumar22805-sys <amankumar22805@gmail.com>
2026-06-05 11:25:15 +0530

    Create product.jsx

---
+++ scr/pages/product.jsx
@@ -0,0 +1,52 @@
+export default function Product(){
+
+return(
+
+<div className="p-5 bg-gray-100 min-h-screen">
+
+<div className="bg-white rounded shadow p-5">
+
+
+<div className="text-7xl text-center">
+👕
+</div>
+
+
+<h1 className="text-3xl font-bold mt-5">
+Custom T-Shirt
+</h1>
+
+
+<p className="mt-3">
+अपनी फोटो और डिजाइन के साथ अपनी T-Shirt बनाएं।
+</p>
+
+
+<h2 className="text-2xl font-bold mt-4">
+₹499
+</h2>
+
+
+<a
+href="/designer"
+className="bg-pink-600 text-white p-3 rounded block text-center mt-5"
+>
+Customize Now
+</a>
+
+
+<a
+href="/cart"
+className="border border-pink-600 text-pink-600 p-3 rounded block text-center mt-3"
+>
+Add To Cart
+</a>
+
+
+</div>
+
+</div>
+
+)
+
+}
