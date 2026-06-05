7c4f0fa9f56fa4f05d3d2728218b6cc1a0dd19e9
amankumar22805-sys <amankumar22805@gmail.com>
2026-06-05 11:05:28 +0530

    Add Checkout component with order details and payment button

---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,46 @@
+export default function Checkout(){
+
+return(
+<div className="p-5">
+
+<h1 className="text-3xl font-bold">
+Checkout
+</h1>
+
+
+<div className="shadow p-5 rounded mt-5">
+
+<h2 className="text-xl">
+Order Details
+</h2>
+
+<p className="mt-3">
+Custom Product Total: ₹499
+</p>
+
+
+<input
+className="border p-2 mt-3 w-full"
+placeholder="Full Name"
+/>
+
+
+<input
+className="border p-2 mt-3 w-full"
+placeholder="Delivery Address"
+/>
+
+
+<button
+className="bg-pink-600 text-white p-3 rounded mt-5 w-full"
+>
+Pay Now (Prepaid Only)
+</button>
+
+
+</div>
+
+</div>
+)
+
+}
