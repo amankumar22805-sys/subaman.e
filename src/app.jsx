6daa0bf76bb4f921a8b66f49cfb0f3b17649ff8f
amankumar22805-sys <amankumar22805@gmail.com>
2026-06-05 10:36:27 +0530

    Set up routing with BrowserRouter and Routes

--- src/app.jsx
+++ src/app.jsx
@@ -1,1 +1,27 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Designer from "./pages/Designer";
+import Navbar from "./components/Navbar";
 
+function App(){
+ return(
+  <BrowserRouter>
+
+   <Navbar/>
+
+   <Routes>
+
+    <Route path="/" element={<Home/>}/>
+
+    <Route 
+    path="/designer" 
+    element={<Designer/>}
+    />
+
+   </Routes>
+
+  </BrowserRouter>
+ )
+}
+
+export default App;
