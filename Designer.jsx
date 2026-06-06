42d6f2304baa090a78f4ef992e9806d21619fc7c
amankumar22805-sys <amankumar22805@gmail.com>
2026-06-06 11:17:45 +0530

    Update Designer.jsx

--- Designer.jsx
+++ Designer.jsx
@@ -1,45 +1,82 @@
 import {useState} from "react";
 
+
 export default function Designer(){
 
 const [image,setImage]=useState(null);
 
+
 function upload(e){
- const file=e.target.files[0];
- if(file){
-  setImage(URL.createObjectURL(file));
- }
+
+const file=e.target.files[0];
+
+if(file){
+setImage(URL.createObjectURL(file));
 }
 
+}
+
+
 return(
-<div className="p-5">
+
+<div className="p-5 bg-gray-100 min-h-screen">
+
 
 <h1 className="text-3xl font-bold">
 Custom Design
 </h1>
 
-<input 
+
+<div className="bg-white shadow rounded p-5 mt-5">
+
+
+<h2 className="text-xl font-bold">
+Upload Your Photo
+</h2>
+
+
+<input
+
 type="file"
+
+accept="image/*"
+
 onChange={upload}
-className="mt-5"
+
+className="mt-4"
+
 />
 
-<div className="mt-5">
 
-{image && 
-<img 
+
+{image &&
+
+<img
+
 src={image}
-className="w-60"
+
+className="w-64 mt-5 rounded"
+
 />
+
 }
 
-</div>
 
-<button className="bg-pink-600 text-white p-3 rounded mt-5">
-Add To Cart (Prepaid)
+
+<button
+
+className="bg-pink-600 text-white p-3 rounded mt-5 w-full"
+
+>
+Create My Product
 </button>
 
+
 </div>
+
+
+</div>
+
 )
 
 }
