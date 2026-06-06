31f0ace99206152336eb690b4af07badfa0e86d0
amankumar22805-sys <amankumar22805@gmail.com>
2026-06-06 11:38:05 +0530

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,8 @@
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 
-import Home from "./pages/Home";
-import Designer from "./pages/Designer";
-import Cart from "./pages/Cart";
-import Checkout from "./pages/Checkout";
-import Login from "./pages/Login";
-import Product from "./pages/Product";
-import Admin from "./pages/Admin";
-
-import Navbar from "./components/Navbar";
-
+import Home from "./pages/product";
+import Login from "./Login";
+import Designer from "./Designer";
 
 function App(){
 
@@ -17,25 +10,13 @@ return(
 
 <BrowserRouter>
 
-<Navbar/>
-
 <Routes>
-
 
 <Route path="/" element={<Home/>}/>
 
-<Route path="/designer" element={<Designer/>}/>
-
-<Route path="/cart" element={<Cart/>}/>
-
-<Route path="/checkout" element={<Checkout/>}/>
-
 <Route path="/login" element={<Login/>}/>
 
-<Route path="/product" element={<Product/>}/>
-
-<Route path="/admin" element={<Admin/>}/>
-
+<Route path="/designer" element={<Designer/>}/>
 
 </Routes>
 
