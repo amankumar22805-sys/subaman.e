b878218292b99cc78e8c44aa478b7203cc95df2f
amankumar22805-sys <amankumar22805@gmail.com>
2026-06-06 12:06:21 +0530

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,76 +1,24 @@
 import {HashRouter, Routes, Route} from "react-router-dom";
 
-import Home from "./pages/Home";
-import Designer from "./Designer";
-import Cart from "./pages/Cart";
-import Checkout from "./pages/Checkout";
-import Login from "./Login";
-import Product from "./pages/product";
-import Admin from "./pages/Admin";
-
-import Navbar from "./components/Navbar";
-
-
 function App(){
 
-return(
-
+return (
 <HashRouter>
-
-<Navbar/>
 
 <Routes>
 
 <Route 
 path="/" 
-element={<Home/>}
+element={
+<h1>SabAman.e Website Working</h1>
+}
 />
-
-
-<Route 
-path="/designer" 
-element={<Designer/>}
-/>
-
-
-<Route 
-path="/cart" 
-element={<Cart/>}
-/>
-
-
-<Route 
-path="/checkout" 
-element={<Checkout/>}
-/>
-
-
-<Route 
-path="/login" 
-element={<Login/>}
-/>
-
-
-<Route 
-path="/product" 
-element={<Product/>}
-/>
-
-
-<Route 
-path="/admin" 
-element={<Admin/>}
-/>
-
 
 </Routes>
 
-
 </HashRouter>
-
 )
 
 }
-
 
 export default App;
